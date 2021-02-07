6670ef88537ed005e82739105ac61815f2ed895f
chisaku-dev <isaacto3890@gmail.com>
2021-02-06 19:20:31 -0800

    easybot.py first commit

---
+++ discordbot.py
@@ -0,0 +1,55 @@
+#discord
+import discord
+from discord.ext import commands
+
+import os
+import asyncio
+
+#bot main
+def startup(name, token, prefix):
+    #start bot
+    bot = commands.Bot(
+        command_prefix=prefix,
+        help=f'A {name} bot',
+        case_insensitive=True
+    )
+
+    #embed help
+    class helpcommand(commands.MinimalHelpCommand):
+        async def send_pages(self):
+            destination = self.get_destination()
+            e = discord.Embed(color=0x1ABC9C, description='')
+            for page in self.paginator.pages:
+                e.description += page
+            await destination.send(embed=e)
+
+    bot.help_command = helpcommand()
+
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user.name} - {bot.user.id}')
+        #importing all plugins found in cogs folder
+        cogs = [f for f in os.listdir('./cogs') if f.endswith('.py')]
+        if not len(cogs) == 0:
+            for cog in cogs:
+                try:
+                    print(f'{cog} was detected and initializing')
+                    cog = cog.replace('.py', '')
+                    bot.load_extension(f'cogs.{cog}')
+                except:
+                    print(f'There was an error at {cog}')
+            print('All cogs were initialized successfully')
+        else:
+            print('No plugins were detected')
+        #starting up bot
+        while True:
+            await bot.change_presence(activity=discord.Activity(
+                type = discord.ActivityType.listening,
+                name=(f" {len(bot.guilds)} servers!")))
+            await asyncio.sleep(60)
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        await ctx.send(error)
+    
+    bot.run(token, bot=True, reconnect=True)
---
+++ easybotlauncher.py
@@ -0,0 +1,51 @@
+#ui
+from tkinter import *
+
+#to read savefile
+import sqlite3
+
+import discordbot
+def save_information(name, token, prefix):
+    conn = sqlite3.connect('save_file.db')
+    conn.execute('''CREATE TABLE IF NOT EXISTS saved_information (
+        name text,
+        token text,
+        prefix text);
+        ''')
+    conn.commit()
+    conn.execute('''INSERT INTO saved_information (name, token, prefix)
+        VALUES (?, ?, ?)''', (name, token, prefix))
+    conn.commit()
+    conn.close()
+
+conn = sqlite3.connect('save_file.db')
+try:
+    cur = conn.cursor()
+    cur.execute('SELECT name, token, prefix FROM saved_information;')
+    saved_information_tuple = cur.fetchone()
+    discordbot.startup(saved_information_tuple[0], saved_information_tuple[1], saved_information_tuple[2])
+    #conn.close()
+except sqlite3.Error:
+    #ui
+    #window
+    tkWindow = Tk()  
+    tkWindow.geometry('400x150')  
+    tkWindow.title('easybot.py by Chisaki-Dev')
+    #name label and text entry box
+    nameLabel = Label(tkWindow, text="name").grid(row=0, column=0)
+    name = StringVar()
+    nameEntry = Entry(tkWindow, textvariable=name).grid(row=0, column=1)  
+
+    #token label and text entry box
+    tokenLabel = Label(tkWindow, text="token").grid(row=1, column=0)
+    token = StringVar()
+    tokenEntry = Entry(tkWindow, textvariable=token).grid(row=1, column=1)  
+
+    prefixLabel = Label(tkWindow, text="prefix").grid(row=4, column=0)
+    prefix = StringVar()
+    prefixEntry = Entry(tkWindow, textvariable=prefix).grid(row=4, column=1)
+
+    #startup button
+    startupButton = Button(tkWindow, text="start easybot.py", command=lambda:[tkWindow.withdraw(), save_information(name.get(), token.get(), prefix.get()), discordbot.startup(name.get(), token.get(), prefix.get())]).grid(row=6, column=1)  
+
+    tkWindow.mainloop()
