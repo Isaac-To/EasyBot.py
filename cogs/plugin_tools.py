1ce5697bc96c0cae47e339ecda3cdaf8ff919348
chisaku-dev <isaacto3890@gmail.com>
2021-06-26 19:17:01 -0700

    merge invitation into plugin_tools.py

--- cogs/plugin_tools.py
+++ cogs/plugin_tools.py
@@ -1,10 +1,27 @@
-import os
+import subprocess
 
 def install(library_name):
-    os.system(f'pip install {library_name} /dev/null 2>&1')
-    os.system(f'pip3 install {library_name} /dev/null 2>&1')
+    subprocess.call(f'pip install {library_name}', shell=False)
+    subprocess.call(f'pip3 install {library_name}', shell=False)
     print(f'Library {library_name} has been installed')
 
 def install_multiple(libraries):
     for library in libraries:
         install(library)
+
+from discord.ext import commands
+class Utility(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(
+        name='invite',
+        help='Generates the link to invite bot',
+        pass_context=True
+        )
+    async def invite(self, ctx):
+        await ctx.send(f'https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=8')
+
+def setup(bot):
+    bot.add_cog(Utility(bot))
