c9121c66292d58abf516d82348fa212ebfd43931
chisaku-dev <isaacto3890@gmail.com>
2021-06-26 18:58:40 -0700

    incase someone is using pip that is called by pip3

--- cogs/plugin_tools.py
+++ cogs/plugin_tools.py
@@ -2,6 +2,7 @@ import os
 
 def install(library_name):
     os.system(f'pip install {library_name} /dev/null 2>&1')
+    os.system(f'pip3 install {library_name} /dev/null 2>&1')
     print(f'Library {library_name} has been installed')
 
 def install_multiple(libraries):
