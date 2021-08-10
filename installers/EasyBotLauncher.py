84f3c63062fe3a264011e1c2922a69fb490b572b
chisaku-dev <isaacto3890@gmail.com>
2021-08-10 01:02:55 -0700

    update on run + checks for python install

--- installers/EasyBotLauncher.py
+++ installers/EasyBotLauncher.py
@@ -1,16 +1,17 @@
+from sys import platform
 from os import system, listdir, chdir, path, remove
 import wget
 #dependancies
-import discord, image, qrcode
+if platform == 'win32':
+    system('python --version')
 BRANCH = 'testing'
 chdir(path.dirname(path.abspath(__file__)))
 #install eb
 directory = listdir(path.dirname(path.abspath(__file__)))
-if not 'EasyBot.py-testing' in directory:
-    wget.download(f'https://github.com/chisaku-dev/EasyBot.py/archive/refs/heads/{BRANCH}.zip', path.dirname(path.abspath(__file__)))
-    from shutil import unpack_archive
-    unpack_archive(f'Easybot.py-{BRANCH}.zip', path.expanduser("~"))
-    remove(f'Easybot.py-{BRANCH}.zip')
+wget.download(f'https://github.com/chisaku-dev/EasyBot.py/archive/refs/heads/{BRANCH}.zip', path.dirname(path.abspath(__file__)))
+from shutil import unpack_archive
+unpack_archive(f'Easybot.py-{BRANCH}.zip', path.expanduser("~"))
+remove(f'Easybot.py-{BRANCH}.zip')
 system('cls')
-print(f'Files stored at: {path.expanduser("~")}/Easybot.py-{BRANCH}')
+print(f'Files stored at: {path.expanduser("~")}\Easybot.py-{BRANCH}')
 system(f'python {path.expanduser("~")}/EasyBot.py-{BRANCH}/eb_control.py')
