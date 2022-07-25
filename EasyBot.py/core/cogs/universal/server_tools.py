8529a87a34ae981c98489ea3c811edb3da4c3ce0
Isaac-To <isaacto3890@gmail.com>
2022-07-25 00:18:39 -0700

    fix guild no url

--- EasyBot.py/core/cogs/universal/server_tools.py
+++ EasyBot.py/core/cogs/universal/server_tools.py
@@ -29,7 +29,9 @@ class SUtility(commands.Cog):
         Owned by UID: {owner}\n
         Explicit content filter enabled for {explicit}\n
         '''
-        embed.set_thumbnail(url=guild.icon_url)
+        try:
+            embed.set_thumbnail(url=guild.icon_url)
+        except: pass
         await inter.response.send_message(embed=embed)
 
     @commands.slash_command(description="Deletes messages up to the number inputted")
